d2f54713467fdf328685b64d0a4a62639d5b0c32
RamanMilashevich <roma.milashevich@gmail.com>
2025-04-03 21:56:03 +0200

    feat: add solutiot to getAllResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -171,8 +171,33 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return new Promise((resolve) => {
+    const result = [];
+    let count = 0;
+    const length = Number(promises.length);
+    if (length === 0) {
+      resolve(result);
+      return;
+    }
+    promises.forEach((promise, index) => {
+      promise
+        .then((value) => {
+          result[index] = value;
+          count += 1;
+          if (count === length) {
+            resolve(result);
+          }
+        })
+        .catch(() => {
+          result[index] = null;
+          count += 1;
+          if (count === length) {
+            resolve(result);
+          }
+        });
+    });
+  });
 }
 
 /**
